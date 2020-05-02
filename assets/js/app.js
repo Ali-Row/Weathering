3c045c4cb0de4e2ac41eb376ec7dd0d86523649f
Alistair <deadlyhardware@gmail.com>
2020-05-02 09:58:43 -0400

    Add media query for smaller devices

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,12 +40,12 @@ $(document).ready(function(){
      
             weather.append(`
             <div class="d-flex justify-content-around mt-5">
-                <a href="index.html" class="mt-2"><i class="fas fa-search"></i></a>
-                <h1 class="city text-center"> ${currentCity} </h1>
-                <a href="index.html" class="mt-2"><i class="fas fa-plus"></i></a>
+                <a href="index.html" class="mt-2 animated fadeIn delay-1s"><i class="fas fa-search"></i></a>
+                <h1 class="city text-center animated fadeInDown delay-1s"> ${currentCity} </h1>
+                <a href="index.html" class="mt-2 animated fadeIn delay-1s"><i class="fas fa-plus"></i></a>
             </div>
             `);
-            weather.append(`<h6 class="text-center m-bot-60 time"> ${today}, ${time} </h6>`);
+            weather.append(`<h6 class="text-center m-bot-60 time animated fadeInDown delay-1s"> ${today}, ${time} </h6>`);
 
             switch(sky) {
                 case 'Clouds':
@@ -57,15 +57,16 @@ $(document).ready(function(){
                 case 'Rain':
                     weatherLogo = (`<h1 class="sky ml-5"><i class="weather-logo fas fa-cloud-showers-heavy"></i> Rainy</h1>`); 
                 break;
-            }            
+            }      
+
             weather.append(`
-                <div class="d-flex justify-content-around">
+                <div class="d-flex justify-content-around animated fadeInDown">
                     <div class="text-center">
                         ${weatherLogo}
                         <h1 class="temp ml-5"> ${temp}° </h1>      
                     </div>
                             
-                    <div class="mt-4 mr-5">
+                    <div class="mt-4 mr-5 animated fadeInDown">
                         <h3 class="max-temp"> ${maxTemp}°F </h3>
                         <hr class="w-100">
                         <h3 class="min-temp"> ${minTemp}°F </h3>
@@ -73,38 +74,34 @@ $(document).ready(function(){
                 </div>
             `);
 
-            
-            weather.append(`
-           
-          `)  
-
             weather.append(`
                 <div class="row mt-5 text-center">
-                    <div class="col-md-6">
+                    <div class="col-xl-6">
                         <iframe
-                            class="text-center"
-                            width="500"
-                            height="400"
-                            frameborder="0" style="border:0; max-width: 400px;"
+                            class="text-center animated fadeIn delay-1s"
+                            frameborder="0" style="border:0;"
                             src="https://www.google.com/maps/embed/v1/place?key=AIzaSyClFZgU_nLZ7QpoqQC_IvIizDwaNpYsYsU
                             &q=${city},${state}" allowfullscreen>
                         </iframe>
                     </div>
 
-                    <div class="col-md-6">
+                    <div class="col-xl-6 animated fadeIn delay-1s">
                         <div class="col-md-4 m-auto extra-info">
-                            <i class="fas fa-tint"></i>
-                            <h4>${humidity}% </h4>
+                            <i class="fas fa-tint m-3"></i>
+                            <h4> Humidity </h4>
+                            <h2>${humidity}%</h2>
                         </div>
 
                         <div class="col-md-4 m-auto extra-info">
-                            <i class="fas fa-thermometer-quarter"></i>
-                            <h4>${feelsLike}°F </h4>
+                            <i class="fas fa-thermometer-quarter m-3"></i>
+                            <h4> Feels Like </h4>
+                            <h2>${feelsLike}°F</h2>
                         </div>
 
                         <div class="col-md-4 m-auto extra-info">
-                            <i class="fas fa-wind"></i>
-                            <h4>${windSpeed} mph </h4>
+                            <i class="fas fa-wind m-3"></i>
+                            <h4> Wind </h4>
+                            <h2>${windSpeed} mph </h2>
                         </div>
                     </div>
                 </div>
